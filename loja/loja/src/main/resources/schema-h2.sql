ed4732b65c834c957c647b3aa2c307bd7543a7b0
Thiago Souto <thiagocsouto@outlook.com>
2022-03-03 12:59:40 -0400

    Delete loja/loja directory

--- loja/loja/src/main/resources/schema-h2.sql
+++
@@ -1,5 +0,0 @@
-create table produto(id integer auto_increment, nome varchar(50), preço varchar(20), categoria varchar(50), quantidade integer);
-
-create table cliente(id integer auto_increment, nome varchar(50), rg varchar(15), idade integer);   
- 
-create table funcionario(id integer auto_increment, nome varchar(50), matricula varchar(20), cpf varchar(20), cargo varchar(20), admissao varchar(20)); 
