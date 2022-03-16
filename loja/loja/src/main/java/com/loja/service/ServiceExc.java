184b6d35c8bb8dfb4bad254cbcbb443e03c897e6
Thiago Souto <thiagocsouto@outlook.com>
2022-03-16 13:32:52 -0400

    Add files via upload

---
+++ loja/loja/src/main/java/com/loja/service/ServiceExc.java
@@ -0,0 +1,11 @@
+package com.loja.service;
+
+public class ServiceExc extends Exception {
+	
+	public ServiceExc(String message) {
+		super(message);
+	}
+	
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ loja/loja/src/main/resources/static/js/main.js
@@ -0,0 +1,6 @@
+function GerarMatricula(){
+	var txt = "F";
+	var aleatorio = Math.floor(Math.random() * 20000);
+	document.getElementById('matricula').value = (txt + 0 + aleatorio);
+}
+
