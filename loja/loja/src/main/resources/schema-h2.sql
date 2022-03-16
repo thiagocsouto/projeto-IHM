2e59de23de046959f7a8dcaf6812c15908f3729e
Thiago Souto <thiagocsouto@outlook.com>
2022-03-16 12:02:59 -0400

    Delete loja/loja directory

--- loja/loja/src/main/resources/schema-h2.sql
+++
@@ -1,7 +0,0 @@
-create table produto(id integer auto_increment, nome varchar(50), preço varchar(20), categoria varchar(50), quantidade integer);
-
-create table cliente(id integer auto_increment, nome varchar(50), cpf varchar(18), sexo varchar(15), idade integer);   
- 
-create table funcionario(id integer auto_increment, nome varchar(50), matricula varchar(20), cpf varchar(20), sexo varchar(20), cargo varchar(20), admissao varchar(20));
-
-create table usuario(id integer auto_increment, login varchar(25), senha varchar(40));
--- loja/loja/target/classes/static/js/main.js
+++
@@ -1,6 +0,0 @@
-function GerarMatricula(){
-	var txt = "T";
-	var aleatorio = Math.floor(Math.random() * 1500000);
-	document.getElementById('matricula').value(txt + aleatorio);
-}
-
