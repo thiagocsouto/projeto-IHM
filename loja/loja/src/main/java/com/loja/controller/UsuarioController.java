d14e0af628ce5a7a57b8f40c40807ba0d044285a
Thiago Souto <thiagocsouto@outlook.com>
2022-04-19 13:21:15 -0500

    Add files via upload

---
+++ loja/loja/src/main/java/com/loja/controller/UsuarioController.java
@@ -0,0 +1,50 @@
+package com.loja.controller;
+
+import java.security.NoSuchAlgorithmException;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.loja.model.Produto;
+import com.loja.model.Usuario;
+import com.loja.repository.UsuarioRepository;
+
+@Controller
+public class UsuarioController {
+	
+	@Autowired
+	private UserDetailsService usuarioService;
+	
+	@GetMapping("/login") 
+	public ModelAndView login() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("login/login");
+		mv.addObject("usuario", new Usuario());  
+		return mv;  
+	} 
+	 
+	@GetMapping("/home") 
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView(); 
+		mv.setViewName("home/index");
+		mv.addObject("produto", new Produto());  
+		return mv;   
+	}  
+	
+//	
+//	@PostMapping("/logout")
+//	public ModelAndView logout(HttpSession session) {
+//		session.invalidate();
+//		return login();
+//	}
+}
+	
+
